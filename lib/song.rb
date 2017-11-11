33e11afeeffb772bdf160ddd49b550d75c049ee8
Nolan Byrnes <nolanehs@gmail.com>
2017-11-11 13:36:02 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -29,11 +29,11 @@ def self.create_by_name(name)
 end
 
 def self.find_by_name(name)
-self.all.detect{|song| song.name ==name}
+self.all.detect{|song| song.name == name}
 end
 
 def self.find_or_create_by_name(name)
-  self.all.detect{|song| song.name ==name} || self.create_by_name(name)
+  self.all.detect{|song| song.name == name} || self.create_by_name(name)
 end
 
 
